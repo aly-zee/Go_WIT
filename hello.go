5d9c1fae15668386964e11497930bf0ee235be4b
azmacias <azmacias05@gmail.com>
2021-06-30 16:39:51 -0700

    initial

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variables to display
+	myName := "Alejandra"
+	myCity := "Santa Clarita"
+	myState := "California"
+	// display name, city, and state
+	fmt.Println("Hello World!")
+	fmt.Println("My name is " + myName)
+	fmt.Println("I live in " + myCity + ", " + myState)
+}
